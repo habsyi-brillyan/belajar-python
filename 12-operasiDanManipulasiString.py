fea70ef3ab6d0d70a3be3323b5361105c3dc389d
habsyi-brillyan <brillyan29806@gmail.com>
2026-08-26 01:30:30 +0700

    Menambahkan file operasi dan manipulasi string

---
+++ 12-operasiDanManipulasiString.py
@@ -0,0 +1,11 @@
+# Operasi dan manipulasi string
+
+# Menyambung string
+nama_depan = 'ucok'
+nama_tengah = 'bin'
+nama_belakang = 'mail'
+nama_lengkap = nama_depan + ' ' + nama_tengah + ' ' + nama_belakang
+print('\nnama depan', nama_depan)
+print('\nnama tengah', nama_tengah)
+print('\nnama belakang', nama_belakang)
+print('\nnama lengkap', nama_lengkap)
